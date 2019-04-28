80cafafc3b74ead6bf823d66e0b7a6cc1dbcaa7f
Marco Loew <marcoloew333@gmail.com>
2019-04-28 18:58:19 +0200

    Imported numpy, pandas and matplotlib.pyplot Added import of new csv-file Added print out of "BG"-row Added print out of "Purger"-column

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("test.csv", index_col=0)
-print(data[["BG"], :])
-print(data[:, ["Purger"]])
+data = pd.read_csv("geraeterohdaten.csv", sep=";", index_col=0)
+# print(data)
+print(data.loc[["BGA"], :])
+# print(data[:, ["Purger"]])
